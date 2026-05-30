1a045ee97ad2599057cd37c422fd7b7290248e7a
ZeLyeVaR <kimslava0107@mail.ru>
2026-05-30 14:26:06 +0300

    laravel+react points

--- laravel/bootstrap/app.php
+++ laravel/bootstrap/app.php
@@ -7,14 +7,19 @@ use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-    web: __DIR__.'/../routes/web.php',
-    api: __DIR__.'/../routes/api.php',
-    commands: __DIR__.'/../routes/console.php',
-    health: '/up',
-    )   
+        web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
+        commands: __DIR__.'/../routes/console.php',
+        health: '/up',
+    )
     ->withMiddleware(function (Middleware $middleware): void {
-    $middleware->alias([
-        'role' => \App\Http\Middleware\RoleMiddleware::class,
+
+        // ✅ CORS (ВАЖНО для React)
+        $middleware->append(\Illuminate\Http\Middleware\HandleCors::class);
+
+        // твой role middleware
+        $middleware->alias([
+            'role' => \App\Http\Middleware\RoleMiddleware::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
